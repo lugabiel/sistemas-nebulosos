f02bb52e0b4bdc679217a5d36be7d78d2aa04c78
lugabiel <36977837+lugabiel@users.noreply.github.com>
2021-09-11 21:09:43 -0300

    Add solucao1.m e solucao2.m
    Arquivos .slx não podem ser utilizados para inspecionar iterações no desenvolvimento, por isso os novos arquivos .m

---
+++ parte1-curso/solucao1.m
@@ -0,0 +1,5 @@
+%%  Sistemas Nebulosos - UFMG 2021 - Luiz Gabriel Aragão 
+%   Exercícios computacionais - Parte 1
+%   Sistemas Nebulosos 
+
+
---
+++ parte2-curso/solucao2.m
@@ -0,0 +1,3 @@
+%%  Sistemas Nebulosos - UFMG 2021 - Luiz Gabriel Aragão 
+%   Exercícios computacionais - Parte 1
+%   Sistemas Nebulosos 
